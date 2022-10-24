e39efa9a9cd6daec9f5e2f2796319ee694ad8673
brolly <102495553@qq.com>
2022-10-24 14:31:26 +0800

    feat: three.js动画

--- src/App.jsx
+++ src/App.jsx
@@ -8,10 +8,11 @@ import {
 import Home from './pages/Home'
 import Sec from './pages/Sec'
 import Thr from './pages/Thr'
+import Camera from './pages/Camera'
+import Animation from './pages/Animation'
 import './App.css'
 
 function App() {
-
   return (
     <div className="App">
         <a target={'_blank'} href={'https://www.yuque.com/renshengzhiruchujian/occ1eb/'}>《three教程文档》</a>
@@ -27,6 +28,12 @@ function App() {
                     <li>
                         <Link to="/thr">第三个3D场景</Link>
                     </li>
+                    <li>
+                        <Link to="/camera">相机</Link>
+                    </li>
+                    <li>
+                        <Link to="/animation">动画</Link>
+                    </li>
                 </ul>
 
                 <hr />
@@ -34,6 +41,8 @@ function App() {
                     <Route path="/" element={<Home />} />
                     <Route path="/sec" element={<Sec />} />
                     <Route path="/thr" element={<Thr />} />
+                    <Route path="/camera" element={<Camera />} />
+                    <Route path="/animation" element={<Animation />} />
                 </Routes>
             </div>
         </Router>
--- src/pages/Thr.jsx
+++ src/pages/Thr.jsx
@@ -11,7 +11,7 @@ const Thr = () => {
         // 创建一个立方缓冲几何体
         const geometry = new THREE.BoxGeometry(100, 100, 100)
         // 创建材质
-        const material = new THREE.MeshLambertMaterial({color: 0x00ff00})
+        const material = new THREE.MeshLambertMaterial({color: 0xffffff})
         // 生成带有材质的物体
         const cube = new THREE.Mesh(geometry, material)
         // 把物体添加进场景中
@@ -21,7 +21,7 @@ const Thr = () => {
         const ambientLight = new THREE.AmbientLight(0x444444)
         scene.add(ambientLight)
         // 平行光，类似于生活中的太阳光
-        const directionalLight = new THREE.DirectionalLight(0x00ff00, 1)
+        const directionalLight = new THREE.DirectionalLight(0xff0000, 1)
         directionalLight.position.set(400, 200, 300)
         scene.add(directionalLight)
         /** 相机设置 */
